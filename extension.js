9fe197aee6a70d71e891243f4b7f4dbf96b3edfe
Junsang Mun <mun.js@aol.com>
2022-11-12 11:30:36 +0900

    [feature] exception handling, request user input
    - exception handling: do not run when input is not proper editor
    - request user input: to type boj question number manually
    - and ... : cool ascii art!

--- extension.js
+++ extension.js
@@ -1,24 +1,15 @@
 const vscode = require('vscode');
 
 function activate(context) {
-	console.log('Congratulations, your extension "auto-boj-commenter" is now active!');
 	let disposable = vscode.commands.registerCommand('auto-boj-commenter.commentboj', function () {
 
 		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage(`정상적인 editor 창이 아닙니다.`);
+			return ;
+		}
 		let lang = editor.document.languageId;
 		let commentBlock;
-
-		const config = vscode.workspace.getConfiguration('auto-boj-commenter');
-		const creator = config.get('creator');
-
-		const week = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
-		const today = new Date();
-		const date = today.getDate();
-		const month = today.getMonth() + 1;
-		const year = today.getFullYear();
-		const day = week[today.getDay()];
-		console.log(today.getDay());
-		const fullToday = `${year}/${month}/${date} (${day})`;
 
 		switch (true) {
 			case /go/.test(lang):
@@ -40,18 +31,42 @@ function activate(context) {
 				commentBlock = '?';
 				break;
 		}
-		console.log(lang);
+
 		if (commentBlock === '?') {
-			vscode.window.showErrorMessage('지원하지 않는 언어입니다.');
+			vscode.window.showWarningMessage(`langId: ${lang}`);
+			vscode.window.showErrorMessage(`지원하지 않는 언어입니다.`);
 			editor = null;
 		}
+
 		if (editor) {
-			//const doc = editor.document;
-			editor.edit(editBuilder => {
-				editBuilder.insert(new vscode.Position(0, 0), `${commentBlock} Hello World!\n${commentBlock} Created by: ${creator}\n${commentBlock} Created at: ${fullToday}\n\n`);
-			})
+			vscode.window.showInputBox({"title": "Enter BOJ question number", "placeHolder": "Example. 1234"})
+			.then(
+				fulfill => { handleFulfilled(fulfill) },
+				rejected => { handleRejected(rejected) }
+			);
 		}
-		//vscode.window.showInformationMessage('Hello World from Auto BOJ commenter!');
+
+		function handleFulfilled(fulfill) {
+			const config = vscode.workspace.getConfiguration('auto-boj-commenter');
+			const creator = config.get('creator');
+			const week = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+			const today = new Date();
+			const date = today.getDate();
+			const month = today.getMonth() + 1;
+			const year = today.getFullYear();
+			const day = week[today.getDay()];
+			const fullToday = `${year}/${month}/${date} (${day})`;
+
+			if (editor) {
+				//const doc = editor.document;
+				editor.edit(editBuilder => {
+					editBuilder.insert(new vscode.Position(0, 0), `${commentBlock} ██████╗  ██████╗      ██╗\n${commentBlock} ██╔══██╗██╔═══██╗     ██║\n${commentBlock} ██████╔╝██║   ██║     ██║\n${commentBlock} ██╔══██╗██║   ██║██   ██║\n${commentBlock} ██████╔╝╚██████╔╝╚█████╔╝\n${commentBlock} ╚═════╝  ╚═════╝  ╚════╝\n${commentBlock} Created by: ${creator}\n${commentBlock} Created at: ${fullToday}\n${commentBlock} BOJ Number: ${fulfill}\n${commentBlock} https://boj.kr/${fulfill}\n\n`);
+				})
+			}
+		}
+		function handleRejected(reject) {
+			console.error(reject);
+		}
 	});
 
 	context.subscriptions.push(disposable);
