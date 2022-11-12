c0cd3f1e3ba7f8c8edd86ddd5ff4e5ee5c0e7b3f
Junsang Mun <mun.js@aol.com>
2022-11-12 10:41:11 +0900

    [feature] add setting and auto-date
    - get createor name @ vsc setting
    - auto date

--- extension.js
+++ extension.js
@@ -3,9 +3,23 @@ const vscode = require('vscode');
 function activate(context) {
 	console.log('Congratulations, your extension "auto-boj-commenter" is now active!');
 	let disposable = vscode.commands.registerCommand('auto-boj-commenter.commentboj', function () {
+
 		let editor = vscode.window.activeTextEditor;
-		let commentBlock;
 		let lang = editor.document.languageId;
+		let commentBlock;
+
+		const config = vscode.workspace.getConfiguration('auto-boj-commenter');
+		const creator = config.get('creator');
+
+		const week = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+		const today = new Date();
+		const date = today.getDate();
+		const month = today.getMonth() + 1;
+		const year = today.getFullYear();
+		const day = week[today.getDay()];
+		console.log(today.getDay());
+		const fullToday = `${year}/${month}/${date} (${day})`;
+
 		switch (true) {
 			case /go/.test(lang):
 				commentBlock = '//';
@@ -34,7 +48,7 @@ function activate(context) {
 		if (editor) {
 			//const doc = editor.document;
 			editor.edit(editBuilder => {
-				editBuilder.insert(new vscode.Position(0, 0), `${commentBlock} Hello World!\n`);
+				editBuilder.insert(new vscode.Position(0, 0), `${commentBlock} Hello World!\n${commentBlock} Created by: ${creator}\n${commentBlock} Created at: ${fullToday}\n\n`);
 			})
 		}
 		//vscode.window.showInformationMessage('Hello World from Auto BOJ commenter!');
